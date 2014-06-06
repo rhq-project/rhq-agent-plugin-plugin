f656d0a129102053b3fe3d4cdea025f834b29286
Thomas Segismont <tsegismo@redhat.com>
2014-06-06 18:22:01 +0200

    Closes #16 Upload mojo: RHQ4.12 allows to wait for plugin scan and plugins update on all agents

--- src/main/java/org/rhq/maven/plugins/UploadMojo.java
+++ src/main/java/org/rhq/maven/plugins/UploadMojo.java
@@ -1,6 +1,6 @@
 /*
  * RHQ Management Platform
- * Copyright 2013, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2013-2014, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -26,16 +26,16 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.AbortableHttpRequest;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.FileEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.BasicClientConnectionManager;
@@ -45,12 +45,17 @@ import org.apache.http.util.EntityUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import static java.lang.Boolean.TRUE;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.apache.http.HttpHeaders.ACCEPT;
+import static org.apache.http.entity.ContentType.APPLICATION_JSON;
+import static org.apache.http.entity.ContentType.APPLICATION_OCTET_STREAM;
+import static org.apache.maven.plugins.annotations.LifecyclePhase.PACKAGE;
 import static org.rhq.maven.plugins.Utils.getAgentPluginArchiveFile;
 
 /**
@@ -58,14 +63,14 @@ import static org.rhq.maven.plugins.Utils.getAgentPluginArchiveFile;
  *
  * @author Thomas Segismont
  */
-@Mojo(name = "upload", defaultPhase = LifecyclePhase.PACKAGE, threadSafe = true)
+@Mojo(name = "upload", defaultPhase = PACKAGE, threadSafe = true)
 public class UploadMojo extends AbstractMojo {
 
-    private static final String REST_CONTENT_URI = "/rest/content";
-    private static final String UPLOAD_URI = REST_CONTENT_URI + "/fresh";
-
-    private static final int SOCKET_CONNECTION_TIMEOUT = 1000 * 30;
-    private static final int SOCKET_READ_TIMEOUT = 1000 * 30;
+    private static final String REST_CONTEXT_PATH = "/rest";
+    private static final String REST_CONTENT_URI = REST_CONTEXT_PATH + "/content";
+    private static final String REST_CONTENT_UPLOAD_URI = REST_CONTENT_URI + "/fresh";
+    private static final String REST_PLUGINS_URI = REST_CONTEXT_PATH + "/plugins";
+    private static final String REST_PLUGINS_DEPLOY_URI = REST_PLUGINS_URI + "/deploy";
 
     /**
      * The build directory (root of build works).
@@ -116,6 +121,25 @@ public class UploadMojo extends AbstractMojo {
     private boolean startScan;
 
     /**
+     * Whether all agents should update their plugins. <strong>This will make your agents' plugin containers restart
+     * .</strong>
+     */
+    @Parameter(defaultValue = "false")
+    private boolean updatePluginsOnAllAgents;
+
+    /**
+     * Whether to wait for the plugins update requests to complete.
+     */
+    @Parameter(defaultValue = "false")
+    private boolean waitForPluginsUpdateOnAllAgents;
+
+    /**
+     * How long should we wait for the plugins update requests to complete. In seconds.
+     */
+    @Parameter(defaultValue = "300")
+    private long maxWaitForPluginsUpdateOnAllAgents;
+
+    /**
      * Whether to fail the build if an error occurs while uploading.
      */
     @Parameter(defaultValue = "true")
@@ -127,6 +151,18 @@ public class UploadMojo extends AbstractMojo {
     @Parameter(defaultValue = "false")
     private boolean skipUpload;
 
+    /**
+     * In milliseconds.
+     */
+    @Parameter(defaultValue = "60000")
+    private int socketConnectionTimeout;
+
+    /**
+     * In milliseconds.
+     */
+    @Parameter(defaultValue = "300000")
+    private int socketReadTimeout;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (skipUpload) {
@@ -137,45 +173,136 @@ public class UploadMojo extends AbstractMojo {
             throw new MojoExecutionException("Agent plugin archive does not exist: " + agentPluginArchive);
         }
 
-        // Prepare HttpClient for plugin upload
+        // Prepare HttpClient
         ClientConnectionManager httpConnectionManager = new BasicClientConnectionManager();
         DefaultHttpClient httpClient = new DefaultHttpClient(httpConnectionManager);
         HttpParams httpParams = httpClient.getParams();
-        HttpConnectionParams.setConnectionTimeout(httpParams, SOCKET_CONNECTION_TIMEOUT);
-        HttpConnectionParams.setSoTimeout(httpParams, SOCKET_READ_TIMEOUT);
+        HttpConnectionParams.setConnectionTimeout(httpParams, socketConnectionTimeout);
+        HttpConnectionParams.setSoTimeout(httpParams, socketReadTimeout);
         httpClient.getCredentialsProvider().setCredentials(new AuthScope(host, port),
                 new UsernamePasswordCredentials(username, password));
 
         HttpPost uploadContentRequest = null;
         HttpPut moveContentToPluginsDirRequest = null;
+        HttpPost pluginScanRequest = null;
+        HttpPost pluginDeployRequest = null;
+        HttpGet pluginDeployCheckCompleteRequest = null;
         try {
+
             // Upload plugin content
             URI uploadContentUri = buildUploadContentUri();
             uploadContentRequest = new HttpPost(uploadContentUri);
-            uploadContentRequest.setEntity(new FileEntity(agentPluginArchive, ContentType.APPLICATION_OCTET_STREAM));
-            uploadContentRequest.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            uploadContentRequest.setEntity(new FileEntity(agentPluginArchive, APPLICATION_OCTET_STREAM));
+            uploadContentRequest.setHeader(ACCEPT, APPLICATION_JSON.getMimeType());
             HttpResponse uploadContentResponse = httpClient.execute(uploadContentRequest);
+
             if (uploadContentResponse.getStatusLine().getStatusCode() != HttpStatus.SC_CREATED) {
                 handleProblem(uploadContentResponse.getStatusLine().toString());
+                return;
             }
+
+            getLog().info("Uploaded " + agentPluginArchive);
+            // Read the content handle value in JSON response
             JSONObject uploadContentResponseJsonObject = new JSONObject(EntityUtils.toString(uploadContentResponse
                     .getEntity()));
-            // Read the content handle value in JSON response 
             String contentHandle = (String) uploadContentResponseJsonObject.get("value");
-            getLog().info("Uploaded " + agentPluginArchive);
+            uploadContentRequest.abort();
 
-            // Request uploaded to be moved to the plugins directory
-            URI moveContentToPluginsDirUri = buildMoveContentToPluginsDirUri(contentHandle);
-            moveContentToPluginsDirRequest = new HttpPut(moveContentToPluginsDirUri);
-            moveContentToPluginsDirRequest.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
-            HttpResponse moveContentToPluginsDirResponse = httpClient.execute(moveContentToPluginsDirRequest);
-            if (moveContentToPluginsDirResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-                handleProblem(moveContentToPluginsDirResponse.getStatusLine().toString());
-            }
-            if (startScan) {
-                getLog().info("Moved uploaded content to plugins directory and triggered plugin scan");
-            } else {
+            if (!startScan && !updatePluginsOnAllAgents) {
+
+                // Request uploaded content to be moved to the plugins directory but do not trigger a plugin scan
+                URI moveContentToPluginsDirUri = buildMoveContentToPluginsDirUri(contentHandle);
+                moveContentToPluginsDirRequest = new HttpPut(moveContentToPluginsDirUri);
+                moveContentToPluginsDirRequest.setHeader(ACCEPT, APPLICATION_JSON.getMimeType());
+                HttpResponse moveContentToPluginsDirResponse = httpClient.execute(moveContentToPluginsDirRequest);
+
+                if (moveContentToPluginsDirResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                    handleProblem(moveContentToPluginsDirResponse.getStatusLine().toString());
+                    return;
+                }
+
+                moveContentToPluginsDirRequest.abort();
                 getLog().info("Moved uploaded content to plugins directory");
+                return;
+            }
+
+            // Request uploaded content to be moved to the plugins directory and trigger a plugin scan
+            URI pluginScanUri = buildPluginScanUri(contentHandle);
+            pluginScanRequest = new HttpPost(pluginScanUri);
+            pluginScanRequest.setHeader(ACCEPT, APPLICATION_JSON.getMimeType());
+            getLog().info("Moving uploaded content to plugins directory and requesting a plugin scan");
+            HttpResponse pluginScanResponse = httpClient.execute(pluginScanRequest);
+
+            if (pluginScanResponse.getStatusLine().getStatusCode() != HttpStatus.SC_CREATED //
+                    && pluginScanResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                handleProblem(pluginScanResponse.getStatusLine().toString());
+                return;
+            }
+
+            pluginScanRequest.abort();
+            getLog().info("Plugin scan complete");
+
+            if (updatePluginsOnAllAgents) {
+
+                URI pluginDeployUri = buildPluginDeployUri();
+                pluginDeployRequest = new HttpPost(pluginDeployUri);
+                pluginDeployRequest.setHeader(ACCEPT, APPLICATION_JSON.getMimeType());
+                getLog().info("Requesting agents to update their plugins");
+                HttpResponse pluginDeployResponse = httpClient.execute(pluginDeployRequest);
+
+                if (pluginDeployResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                    handleProblem(pluginDeployResponse.getStatusLine().toString());
+                    return;
+                }
+
+                getLog().info("Plugins update requests sent");
+                // Read the agent plugins update handle value in JSON response
+                JSONObject pluginDeployResponseJsonObject = new JSONObject(EntityUtils.toString(pluginDeployResponse
+                        .getEntity()));
+                String pluginsUpdateHandle = (String) pluginDeployResponseJsonObject.get("value");
+                pluginDeployRequest.abort();
+
+                if (waitForPluginsUpdateOnAllAgents) {
+
+                    getLog().info("Waiting for plugins update requests to complete");
+
+                    long start = System.currentTimeMillis();
+                    for (; ; ) {
+
+                        URI pluginDeployCheckCompleteUri = buildPluginDeployCheckCompleteUri(pluginsUpdateHandle);
+                        pluginDeployCheckCompleteRequest = new HttpGet(pluginDeployCheckCompleteUri);
+                        pluginDeployCheckCompleteRequest.setHeader(ACCEPT, APPLICATION_JSON.getMimeType());
+                        HttpResponse pluginDeployCheckCompleteResponse = httpClient.execute
+                                (pluginDeployCheckCompleteRequest);
+
+                        if (pluginDeployCheckCompleteResponse.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+                            handleProblem(pluginDeployCheckCompleteResponse.getStatusLine().toString());
+                            return;
+                        }
+
+                        // Read the agent plugins update handle value in JSON response
+                        JSONObject pluginDeployCheckCompleteResponseJsonObject = new JSONObject(EntityUtils.toString
+                                (pluginDeployCheckCompleteResponse.getEntity()));
+                        Boolean pluginDeployCheckCompleteHandle = (Boolean)
+                                pluginDeployCheckCompleteResponseJsonObject.get("value");
+                        pluginDeployCheckCompleteRequest.abort();
+
+                        if (pluginDeployCheckCompleteHandle == TRUE) {
+                            getLog().info("All agents updated their plugins");
+                            return;
+                        }
+
+                        if (SECONDS.toMillis(maxWaitForPluginsUpdateOnAllAgents) < (System.currentTimeMillis() -
+                                start)) {
+                            handleProblem("Not all agents updated their plugins but wait limit has been reached (" +
+                                    maxWaitForPluginsUpdateOnAllAgents + " ms)");
+                            return;
+                        }
+
+                        Thread.sleep(SECONDS.toMillis(5));
+                        getLog().info("Checking plugins update requests status again");
+                    }
+                }
             }
         } catch (IOException e) {
             handleException(e);
@@ -183,14 +310,21 @@ public class UploadMojo extends AbstractMojo {
             handleException(e);
         } catch (URISyntaxException e) {
             handleException(e);
+        } catch (InterruptedException e) {
+            handleException(e);
         } finally {
-            if (uploadContentRequest != null) {
-                uploadContentRequest.abort();
-            }
-            if (moveContentToPluginsDirRequest != null) {
-                moveContentToPluginsDirRequest.abort();
-            }
+            abortQuietly(uploadContentRequest);
+            abortQuietly(moveContentToPluginsDirRequest);
+            abortQuietly(pluginScanRequest);
+            abortQuietly(pluginDeployRequest);
+            abortQuietly(pluginDeployCheckCompleteRequest);
             httpConnectionManager.shutdown();
+        }
+    }
+
+    private void abortQuietly(AbortableHttpRequest httpRequest) {
+        if (httpRequest != null) {
+            httpRequest.abort();
         }
     }
 
@@ -213,7 +347,7 @@ public class UploadMojo extends AbstractMojo {
                 .setScheme(scheme) //
                 .setHost(host) //
                 .setPort(port) //
-                .setPath(UPLOAD_URI) //
+                .setPath(REST_CONTENT_UPLOAD_URI) //
                 .build();
     }
 
@@ -224,7 +358,36 @@ public class UploadMojo extends AbstractMojo {
                 .setPort(port) //
                 .setPath(REST_CONTENT_URI + "/" + contentHandle + "/plugins") //
                 .setParameter("name", getAgentPluginArchiveFile(buildDirectory, finalName).getName()) //
-                .setParameter("startScan", String.valueOf(startScan)) //
+                .setParameter("startScan", String.valueOf(false)) //
+                .build();
+    }
+
+    private URI buildPluginScanUri(String contentHandle) throws URISyntaxException {
+        return new URIBuilder() //
+                .setScheme(scheme) //
+                .setHost(host) //
+                .setPort(port) //
+                .setPath(REST_PLUGINS_URI) //
+                .setParameter("handle", contentHandle) //
+                .setParameter("name", getAgentPluginArchiveFile(buildDirectory, finalName).getName()) //
+                .build();
+    }
+
+    private URI buildPluginDeployUri() throws URISyntaxException {
+        return new URIBuilder() //
+                .setScheme(scheme) //
+                .setHost(host) //
+                .setPort(port) //
+                .setPath(REST_PLUGINS_DEPLOY_URI) //
+                .build();
+    }
+
+    private URI buildPluginDeployCheckCompleteUri(String pluginsUpdateHandle) throws URISyntaxException {
+        return new URIBuilder() //
+                .setScheme(scheme) //
+                .setHost(host) //
+                .setPort(port) //
+                .setPath(REST_PLUGINS_DEPLOY_URI + "/" + pluginsUpdateHandle) //
                 .build();
     }
 }
